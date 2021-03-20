f76f4f6596c0e9b691b6fb1c74c9ea45c2fdbc66
Recomagnesius <buzhor.danyl@gmail.com>
2021-03-20 21:36:49 +0200

    feat: add random command

--- main.js
+++ main.js
@@ -3,17 +3,31 @@ const bot = new Discord.Client();
 let config = require("./botconfig.json");
 let token = config.token;
 let prefix = config.prefix;
-//создаём ссылку-приглашение для бота
-bot.on("ready", () => {
+let guilds = {};
+//Запуск бота
+bot.on("ready", (message) => {
   console.log(`Запустился бот ${bot.user.username}`);
-  bot.generateInvite(["ADMINISTRATOR"]).then((link) => {
-    console.log(link);
-  });
 });
-//команда, и то, что она должна выполнить
+
+//команды
+let insults = [
+  "Иди нахуй",
+  "В рот твою мамку ебал",
+  "ебать ты лох",
+  "такое чмо еще поискать надо",
+  "Да ты пидор а не человек",
+  "продолжай",
+  "ну а хули",
+  "пиздуй отсюдаво",
+  "я тебя на ноль помножу",
+  "пиздишь как дышишь",
+];
 bot.on("message", (msg) => {
-  if (msg.content === prefix + "test") {
-    msg.reply("Fack you!");
+  let check = msg.content;
+  check = String(check).split(" ");
+  if (check[0] === prefix + "random") {
+    msg.reply(insults[Math.floor(Math.random() * insults.length)]);
   }
 });
+//логин бота
 bot.login(token);
